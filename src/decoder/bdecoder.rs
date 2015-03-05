8bed6a59097b12fa21e68a1f14f5f43dc12491d1
Gaetan André <rvlander@gaetanandre.eu>
2015-03-05 07:40:46 +0100

    NEW: parse strings

--- src/decoder/bdecoder.rs
+++ src/decoder/bdecoder.rs
@@ -2,7 +2,7 @@ use super::super::btree::bvalue::BValue;
 use std::str::Chars;
 
 struct BDecoder <'a> {
-	to_parse: Chars<'a>,
+	to_parse: Chars<'a>
 }
 
 impl  <'a> BDecoder <'a> {
@@ -14,14 +14,7 @@ impl  <'a> BDecoder <'a> {
 	}
 
 	fn parse (&mut self) -> Result<BValue, &str> {
-		self.inner_parse(None)
-	}
-
-	fn inner_parse (&mut self, cin: Option<char>) -> Result<BValue, &str> {
-		let opt = match cin {
-			Some(c) => Some(c),
-			None => self.to_parse.next(),
-		};	
+		let opt = self.to_parse.next();
 		match opt {
 			Some(c) => match c {
 				'd' => self.parse_dictionnary(),
@@ -51,21 +44,43 @@ impl  <'a> BDecoder <'a> {
 		let integer: String = self.to_parse.by_ref().take_while(|&c| c != 'e').collect();
 		match integer.as_slice().parse::<i64>() {
 			Ok(a) => Ok(BValue::Integer(a)),
-			Err(err) => Err("Error: bcode could not be parsed: integer malformed"),
+			Err(_) => Err("Error: bcode could not be parsed: integer malformed"),
 		}	
 	}
 
-	fn parse_list(&self) -> Result<BValue, &str> {
+	fn parse_list(&mut self) -> Result<BValue, &str> {
 		Err("Error: bcode could not be parsed: not supported yet !")
 	}
 
-	fn parse_string(&self, cin: char) -> Result<BValue, &str> {
-		Err("Error: bcode could not be parsed: not supported yet !")
+	fn parse_string(&mut self, cin: char) -> Result<BValue, &str> {
+		let mut semi = 'x';
+		let mut tail: String = self.to_parse.by_ref().take_while(|&c| {semi = c; is_num(c)}).collect();
+		tail.insert(0, cin);
+		match tail.as_slice().parse::<usize>() {
+			Ok(a) => {
+				if semi !=':' {return Err("Error: bcode could not be parsed: ':' expected!")};
+				let res: String = self.to_parse.by_ref().take(a).collect();
+				Ok(BValue::String(res))
+			},
+			Err(_) => Err("Error: bcode could not be parsed: string size malformed !"), 
+		}
 	}
 }
 
-#[cfg(test)]
+fn is_num(c: char) -> bool {
+	c =='0'
+	|| c == '1'
+	|| c == '2'
+	|| c == '3'
+	|| c == '4'
+	|| c == '5'
+	|| c == '6'
+	|| c == '7'
+	|| c == '8'
+	|| c == '9'
+}
 
+#[cfg(test)]
 mod test {
 	use super::BDecoder;
 	use super::super::super::btree::bvalue::BValue;
@@ -86,5 +101,14 @@ mod test {
 	fn test_parse_integer() {
 		let mut decoder = BDecoder::new("i128e");
 		assert_eq!(decoder.parse(), Ok(BValue::Integer(128)));	
+	}
+
+	#[test]
+	fn test_parse_string() {
+		let mut decoder = BDecoder::new("4:toto");
+		assert_eq!(decoder.parse(), Ok(BValue::String(String::from_str("toto"))));
+
+		decoder = BDecoder::new("0:");
+		assert_eq!(decoder.parse(), Ok(BValue::String(String::from_str(""))));	
 	}
 }
